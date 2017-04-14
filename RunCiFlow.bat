113bc353cc98e8a50debbdf7daeaa57a8407d32b
Piotr Kowalski <piotr.kowalski@ts.fujitsu.com>
2017-04-14 14:24:30 +0200

    MsBuild config file changes

--- RunCiFlow.bat
+++ RunCiFlow.bat
@@ -1,5 +1,4 @@
 SET msbuild="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 
-%msbuild% "CalculatorAutomatedTests.msbuild" /t:CICompileAndVerify /p:Configuration="Debug" /p:MSTestExePath="null" /p:SolutionName="" /p:ProjectDirectory=""
-
+%msbuild% "CalculatorCIAutomation.msbuild" /t:CICompileAndVerify /p:Configuration="Debug" /p:MSTestExePath="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" /p:ProjectDirectory="C:\Users\KowalskiP\Source\Repos\CITests\CITester"
 pause

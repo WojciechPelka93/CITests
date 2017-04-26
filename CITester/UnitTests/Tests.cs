175bcb823370efc3185cccc8a1208ea006792d10
Piotr Kowalski <piotr.kowalski@ts.fujitsu.com>
2017-04-26 15:25:28 +0200

    Unit Test Samples

---
+++ CITester/UnitTests/Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using WebCoreApplication.Services;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void SampleBusinessLogic_AddTwoNumbers_ShouldAddTwoDoubles() 
+        {
+            // Arrange
+            SampleBusinessLogic sbl = new SampleBusinessLogic();
+            double expected = 2.00, actual;
+
+            // Act
+            actual = sbl.AddTwoNumbers(1.00, 1.00);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ CITester/WebCoreApplication/Services/SampleBusinessLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCoreApplication.Services
+{
+    public class SampleBusinessLogic
+    {
+        public double AddTwoNumbers(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}

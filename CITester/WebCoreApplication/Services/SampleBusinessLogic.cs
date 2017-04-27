2da61d6e3d1498866b135c03b077c58e1099f2c4
Piotr Kowalski <piotr.kowalski@ts.fujitsu.com>
2017-04-27 14:36:53 +0200

    Test

--- CITester/WebCoreApplication/Services/SampleBusinessLogic.cs
+++ CITester/WebCoreApplication/Services/SampleBusinessLogic.cs
@@ -14,7 +14,7 @@ namespace WebCoreApplication.Services
 
         public double MultiplyTwoNumbers(double a, double b)
         {
-            return b * b; 
+            return b * b * b; 
         }
     }
 }
